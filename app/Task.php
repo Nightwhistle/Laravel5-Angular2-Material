232a1f9a8aff239a497163238d27fc65c60ad44a
Nightwhistle <arkantoscg@gmail.com>
2016-11-03 12:50:35 +0100

    Date for humans, Laravel mutators tryout

--- app/Task.php
+++ app/Task.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    protected $dates = ['published_at'];
 
+    public function getCreatedAtAttribute($date) {
+        return Carbon::parse($date)->diffForHumans();
+    }
 }
